7c98d4d2c31ca335f88679302473330e75ee1dc6
Avihay Menahem <avihay@three-dev.com>
2017-02-07 21:24:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,27 @@
 var app = require('koa')(),
     path = require("path"),
     router = require('koa-router')(),
+    nodemailer = require('nodemailer'),
     koaBody = require('koa-body')({ multipart: true,
         formidable: {
             uploadDir: path.join(__dirname, './uploads'),
             keepExtensions : true,
             onFileBegin: (name, file) => {
-                //file.path = "./uploads/" + file.name;
+                file.path = "./uploads/" + file.name;
             }
         } }),
     bodyParser = require('koa-bodyparser'),
     cors = require('koa-cors'),
     Common = require("./Utils/Common").default,
     ENV_PORT = 3002;
+
+let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'nodemailerrpi@gmail.com',
+            pass: '203788583Avm'
+        }
+    });
 
 app.use(cors());
 app.use(bodyParser());
@@ -26,8 +35,24 @@ router.get('/', function *(next) {
 });
 
 router.post('/upload', koaBody, function *(next) {
-    //let slack = new Slack("");
-    console.log("File uploaded");
+
+    let mailOptions = {
+        from: '"Your RPI" <nodemailerrpi@gmail.com>', // sender address
+        to: 'avihay@three-dev.com', // list of receivers
+        subject: 'RPI Public IP Address', // Subject line
+        text: 'Clicktale photo booth image', // plain text body
+        attachments: [{   // file on disk as an attachment
+            path: './uploads/webcam.jpg' // stream this file
+        }]
+    };
+
+// send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log("File uploaded");
+    });
     this.body = "ok";
 });
 
